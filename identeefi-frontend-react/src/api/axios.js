84e3144b8e5bcfc03451d40d959887f57c84fd1b
ayushrakesh <ayushrakesh.05@gmail.com>
2024-07-10 07:03:38 +0530

    backend-localhost

--- identeefi-frontend-react/src/api/axios.js
+++ identeefi-frontend-react/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://identeefi-server-4z5kpc4ar-ayush-rakeshs-projects.vercel.app/'
+    baseURL: 'http://localhost:5000'
 });
